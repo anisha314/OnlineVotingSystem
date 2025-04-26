ee3e1da38d2feb821307369f40a43c0c3cdad01f
ERAGALA JAGAN MOHAN REDDY <eragalajaganmohanreddy1160@gmail.com>
2025-04-26 22:26:26 +0530

    Update CandidateRegister.js

--- src/components/Sign/CandidateRegister.js
+++ src/components/Sign/CandidateRegister.js
@@ -8,7 +8,6 @@ import axios from "axios"
 import { BASE_URL } from "../../helper";
 import { useNavigate } from 'react-router-dom';
 
-
 const CandidateRegister = () => {
     const [loading, setLoading] = useState(false);
 
